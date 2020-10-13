261838b661587feddf762cd72191fcc148736e2d
Baotong LU <baotonglu@Baotongs-MacBook-Pro.local>
2020-10-13 22:52:23 +0800

    reinsert

--- tests/bztree_my_tests.cc
+++ tests/bztree_my_tests.cc
@@ -87,7 +87,7 @@ int main(int argc, char* argv[]){
     for(int i = 0; i < init_num_keys; i++){
         //std::string key = std::to_string(i);
         //printf("insert %d key %.10f, with length %d\n", i, values[i].first, key.length());
-        std::string key = std::to_string(values[i].first);
+        std::string key = std::to_string(values[i].second);
         printf("insert %d key %.10f, with length %d\n", i, values[i].first, key.length());
         //std::cout << "insert value is " << values[i].second << " with size " << sizeof(values[i].second)<< std::endl;
         auto rc = bztree->Insert(key.c_str(), key.length(), i);
